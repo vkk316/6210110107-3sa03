f6bbc5f08d948ae84a02e940d497aac9e1b34e9e
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-11 10:52:25 +0700

    reset and reattemp work

--- src/WordCard.js
+++ src/WordCard.js
@@ -22,8 +22,8 @@ export default function WordCard(props) {
         console.log(`${c} has been activated.`)
         let guess = state.guess + c
         setState({ ...state, guess })
-        if (guess.length == state.word.length) {
-            if (guess == state.word) {
+        if (guess.length === state.word.length) {
+            if (guess === state.word) {
                 console.log('yeah!')
                 setState({ ...state, guess: '', completed: true })
             } else {
@@ -32,10 +32,29 @@ export default function WordCard(props) {
             }
         }
     }
+    
+    const resetGame =() =>{
+        console.log('Reset game')
+        setState(prepareStateFromWord(props.value))
+    }
 
-    return (
-        <div>
-            {state.chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} />)}
-        </div>
-    );
+    if(!state.completed){
+        return (
+            <div>
+                <div> {`Total Attemps: ${state.attempt}`} </div>
+                {state.chars.map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>)}
+            </div>
+        );
+    }else{
+        return(
+            <div>
+                You WIN with {state.attempt} attempts CONGRATS
+                <div>
+                <button onClick={resetGame}>Reset</button>
+                </div>
+
+            </div>
+        );
+    }
+
 }
